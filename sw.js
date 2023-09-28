471899724b31d6aaa809e0df9277c6140ce87f0e
Caio H Perlin <caiohperlin@gmail.com>
2023-09-28 11:29:28 -0400

    bugfix: fix offline fallback

--- sw.js
+++ sw.js
@@ -4,26 +4,6 @@ import { registerRoute, Route } from 'workbox-routing';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
 
-
-const cacheName = "Capivara Notes";
-const filesToCache = ["/", "index.html", "/css/style.css", "/js/db.js", "/js/main.js"];
-
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(filesToCache)
-        })
-    );
-});
-
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
-});
-
 // configurando o cache
 const pageCache = new CacheFirst({
   cacheName: 'capivara-cache',
@@ -42,6 +22,7 @@ warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
+
 //registrando a rota
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
